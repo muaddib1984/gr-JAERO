1a4d9f4bc06d02256269b2ce96413bc5c581651e
Paul Atreides <maud.dib1984@gmail.com>
2022-06-26 00:14:04 -0400

    removed unecessary GUI from subband_channelizer

--- apps/cband_hunter_modular/JAERO_ZMQ_CBAND_Hunter_subband_channelizer.py
+++ apps/cband_hunter_modular/JAERO_ZMQ_CBAND_Hunter_subband_channelizer.py
@@ -10,24 +10,10 @@
 # Description: ZMQ I/Q input, Channelizer and Socket Outputs to JAERO
 # GNU Radio version: 3.10.2.0-rc1
 
-from packaging.version import Version as StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
-
 import os
 import sys
 sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
 
-from PyQt5 import Qt
-from gnuradio import eng_notation
 from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
@@ -35,50 +21,20 @@ from gnuradio.fft import window
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
 from gnuradio import zeromq
 from gnuradio.filter import pfb
-from gnuradio.qtgui import Range, RangeWidget
-from PyQt5 import QtCore
 from pfb_out_to_jaero_zmq import pfb_out_to_jaero_zmq  # grc-generated hier_block
 import JAERO
 import math
 
 
 
-from gnuradio import qtgui
 
-class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block, Qt.QWidget):
+class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block):
 
     def __init__(self, port_start='6001', samp_rate=2.16e6, wtf_port_start='7001', zmq_port=5001):
         gr.top_block.__init__(self, "CBAND HUNTER CHANNELIZER", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("CBAND HUNTER CHANNELIZER")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "JAERO_ZMQ_CBAND_Hunter_subband_channelizer")
-
-        try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
 
         ##################################################
         # Parameters
@@ -104,7 +60,6 @@ class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block, Qt.QWidget):
         self.pfb_out_rate = pfb_out_rate = dec0_rate/pfb_dec
         self.fft_len = fft_len = 8192
         self.wtf_ports = wtf_ports = [i for i in range(int(wtf_port_start),int(wtf_port_start)+15)]
-        self.variable_qtgui_label_0 = variable_qtgui_label_0 = zmq_port
         self.subband_num = subband_num = 1
         self.short_scaling = short_scaling = 32768
         self.rs_ratio = rs_ratio = rs_rate/samp_rate
@@ -121,36 +76,10 @@ class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block, Qt.QWidget):
         self.chan_rate = chan_rate = 10.5e3
         self.bpf = bpf = firdes.complex_band_pass(1.0, out_rate, (usb_bw)*0.05, (usb_bw)*0.95, (usb_bw)*0.05, window.WIN_HAMMING, 6.76)
         self.bin_size = bin_size = (samp_rate/fft_len)
-        self.audio_volume = audio_volume = 100.0
 
         ##################################################
         # Blocks
         ##################################################
-        self.tabs = Qt.QTabWidget()
-        self.tabs_widget_0 = Qt.QWidget()
-        self.tabs_layout_0 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.tabs_widget_0)
-        self.tabs_grid_layout_0 = Qt.QGridLayout()
-        self.tabs_layout_0.addLayout(self.tabs_grid_layout_0)
-        self.tabs.addTab(self.tabs_widget_0, 'JAERO LEVEL CONTROL')
-        self.top_grid_layout.addWidget(self.tabs, 1, 0, 2, 2)
-        for r in range(1, 3):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 2):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self._short_scaling_range = Range(1, 65536, 1, 32768, 200)
-        self._short_scaling_win = RangeWidget(self._short_scaling_range, self.set_short_scaling, "JAERO DIGITAL VOLUME", "counter_slider", int, QtCore.Qt.Horizontal)
-        self.tabs_grid_layout_0.addWidget(self._short_scaling_win, 1, 0, 1, 2)
-        for r in range(1, 2):
-            self.tabs_grid_layout_0.setRowStretch(r, 1)
-        for c in range(0, 2):
-            self.tabs_grid_layout_0.setColumnStretch(c, 1)
-        self._audio_volume_range = Range(0.1, 1000, 0.1, 100.0, 200)
-        self._audio_volume_win = RangeWidget(self._audio_volume_range, self.set_audio_volume, "Audio Signal Volume", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.tabs_grid_layout_0.addWidget(self._audio_volume_win, 0, 0, 1, 2)
-        for r in range(0, 1):
-            self.tabs_grid_layout_0.setRowStretch(r, 1)
-        for c in range(0, 2):
-            self.tabs_grid_layout_0.setColumnStretch(c, 1)
         self.zeromq_sub_source_0 = zeromq.sub_source(gr.sizeof_gr_complex, 1, str("tcp://127.0.0.1:")+str(zmq_port), 100, True, -1, '')
         self.zeromq_pub_sink_0_7 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, str("tcp://127.0.0.1:")+str(wtf_ports[8]), 100, True, -1, '')
         self.zeromq_pub_sink_0_6 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, str("tcp://127.0.0.1:")+str(wtf_ports[7]), 100, True, -1, '')
@@ -167,21 +96,6 @@ class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block, Qt.QWidget):
         self.zeromq_pub_sink_0_0_0 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, str("tcp://127.0.0.1:")+str(wtf_ports[9]), 100, True, -1, '')
         self.zeromq_pub_sink_0_0 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, str("tcp://127.0.0.1:")+str(wtf_ports[1]), 100, True, -1, '')
         self.zeromq_pub_sink_0 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, str("tcp://127.0.0.1:")+str(wtf_ports[0]), 100, True, -1, '')
-        self._variable_qtgui_label_0_tool_bar = Qt.QToolBar(self)
-
-        if None:
-            self._variable_qtgui_label_0_formatter = None
-        else:
-            self._variable_qtgui_label_0_formatter = lambda x: str(x)
-
-        self._variable_qtgui_label_0_tool_bar.addWidget(Qt.QLabel("ZMQ SUBBAND INPUT"))
-        self._variable_qtgui_label_0_label = Qt.QLabel(str(self._variable_qtgui_label_0_formatter(self.variable_qtgui_label_0)))
-        self._variable_qtgui_label_0_tool_bar.addWidget(self._variable_qtgui_label_0_label)
-        self.top_grid_layout.addWidget(self._variable_qtgui_label_0_tool_bar, 0, 0, 1, 1)
-        for r in range(0, 1):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
         self.pfb_out_to_jaero_zmq_0_4 = pfb_out_to_jaero_zmq(
             audio_rate=48e3,
             audio_volume=100.0,
@@ -382,14 +296,6 @@ class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block, Qt.QWidget):
         self.connect((self.zeromq_sub_source_0, 0), (self.pfb_channelizer_ccf_0, 0))
 
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "JAERO_ZMQ_CBAND_Hunter_subband_channelizer")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
-
     def get_port_start(self):
         return self.port_start
 
@@ -418,7 +324,6 @@ class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block, Qt.QWidget):
 
     def set_zmq_port(self, zmq_port):
         self.zmq_port = zmq_port
-        self.set_variable_qtgui_label_0(self.zmq_port)
 
     def get_xlate_rate(self):
         return self.xlate_rate
@@ -549,13 +454,6 @@ class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block, Qt.QWidget):
     def set_wtf_ports(self, wtf_ports):
         self.wtf_ports = wtf_ports
 
-    def get_variable_qtgui_label_0(self):
-        return self.variable_qtgui_label_0
-
-    def set_variable_qtgui_label_0(self, variable_qtgui_label_0):
-        self.variable_qtgui_label_0 = variable_qtgui_label_0
-        Qt.QMetaObject.invokeMethod(self._variable_qtgui_label_0_label, "setText", Qt.Q_ARG("QString", str(self._variable_qtgui_label_0_formatter(self.variable_qtgui_label_0))))
-
     def get_subband_num(self):
         return self.subband_num
 
@@ -668,12 +566,6 @@ class JAERO_ZMQ_CBAND_Hunter_subband_channelizer(gr.top_block, Qt.QWidget):
     def set_bin_size(self, bin_size):
         self.bin_size = bin_size
 
-    def get_audio_volume(self):
-        return self.audio_volume
-
-    def set_audio_volume(self, audio_volume):
-        self.audio_volume = audio_volume
-
 
 
 def argument_parser():
@@ -697,32 +589,21 @@ def argument_parser():
 def main(top_block_cls=JAERO_ZMQ_CBAND_Hunter_subband_channelizer, options=None):
     if options is None:
         options = argument_parser().parse_args()
-
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls(port_start=options.port_start, samp_rate=options.samp_rate, wtf_port_start=options.wtf_port_start, zmq_port=options.zmq_port)
-
-    tb.start()
-
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        Qt.QApplication.quit()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
 
-    qapp.exec_()
+    tb.wait()
+
 
 if __name__ == '__main__':
     main()
